fa33295839f0d62c80a8795d511da9c6b0fee355
Zxurian <zxurian@gmail.com>
2014-10-15 17:19:34 -0400

    Update netsuite-style.js

--- netsuite-style.js
+++ netsuite-style.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Netsuite 2014.2 Style fixer
 // @namespace    https://netsuite.com
-// @version      0.1.5
+// @version      0.1.6
 // @description  Userscript for fixing Netsuite 2014.2 style fixing.
 // @author       Rick Goodrow
 // @homepage     https://github.com/Zxurian/netsuite-style
 // @match        https://*.netsuite.com/*
 // @grant        GM_addStyle
-// @grant        GM_GetResourceText
+// @grant        GM_getResourceText
 // @updateURL    https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.js
 // @downloadURL  https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.js
-// @resource     netsuiteCSS https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.css?0.1.5
+// @resource     netsuiteCSS https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.css?0.1.6
 // ==/UserScript==
 
-GM_addStyle(GM_GetResourceText(netsuiteCSS));
+GM_addStyle(GM_getResourceText(netsuiteCSS));
