a9aeb4e2222cf881420d9c073572d2197765f1c9
Zxurian <zxurian@gmail.com>
2014-10-15 17:18:40 -0400

    Update netsuite-style.js

--- netsuite-style.js
+++ netsuite-style.js
@@ -1,40 +1,16 @@
 // ==UserScript==
 // @name         Netsuite 2014.2 Style fixer
 // @namespace    https://netsuite.com
-// @version      0.1.4
+// @version      0.1.5
 // @description  Userscript for fixing Netsuite 2014.2 style fixing.
 // @author       Rick Goodrow
 // @homepage     https://github.com/Zxurian/netsuite-style
 // @match        https://*.netsuite.com/*
 // @grant        GM_addStyle
+// @grant        GM_GetResourceText
 // @updateURL    https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.js
 // @downloadURL  https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.js
+// @resource     netsuiteCSS https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.css?0.1.5
 // ==/UserScript==
 
-GM_addStyle('
-@media screen {
-	.uir-list-header-td {
-		padding: 1px 5px !important;
-	}
-	.uir-list-row-tr > td {
-		font-size: 12px;
-		padding: 1px 5px !important;
-	}
-}
-
-.x-window-mc {
-	padding: 10px;
-}
-.x-window-mc iframe {
-	height: auto !important;
-}
-
-textarea:disabled {
-	color: rgb(50, 50, 50) !important;
-}
-
-.uir-pagination-select ul {
-	width: auto !important;
-	overflow-x: auto !important;
-}
-');
+GM_addStyle(GM_GetResourceText(netsuiteCSS));
