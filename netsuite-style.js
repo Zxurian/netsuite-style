6ffd345ff29b8ec277787f429e1101bf1a513b99
Zxurian <zxurian@gmail.com>
2014-10-21 10:29:17 -0400

    changing to run at document-start

--- netsuite-style.js
+++ netsuite-style.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Netsuite 2014.2 Style fixer
-// @namespace    https://netsuite.com
-// @version      0.1.12.5
-// @resource     netsuiteCSS https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.css?0.1.12.5
-// @description  Userscript for fixing Netsuite 2014.2 style fixing.
 // @author       Rick Goodrow
 // @homepage     https://github.com/Zxurian/netsuite-style
+// @description  Userscript for fixing Netsuite 2014.2 style fixing.
+// @version      0.2
+// @namespace    https://netsuite.com
+// @run-at       document-start
+// @resource     netsuiteCSS https://raw.githubusercontent.com/Zxurian/netsuite-style/master/netsuite-style.css?0.2
 // @match        https://*.netsuite.com/*
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
